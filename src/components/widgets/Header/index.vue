327a7f2e7cf8d4caef940fc6673ab721c2db81cc
北野 <1017895392@qq.com>
2023-03-14 17:27:25 +0800

    chore: edit

--- src/components/widgets/Header/index.vue
+++ src/components/widgets/Header/index.vue
@@ -2,8 +2,7 @@
   <header class="sticky top-0 flex items-center h-14 sm:px-6 lg:px-8 px-4 bg-white/75 dark:bg-black/75 backdrop-blur-md lg:shadow z-30">
     <Logo />
     <div class="flex-1" />
-    <Search class="hidden md:flex lg:flex" />
-    <Navbar />
+    <Search class="hidden md:flex lg:flex mx-10" />
     <ColorMode />
   </header>
 </template>
@@ -11,7 +10,6 @@
 <script setup lang="ts">
 import Logo from './Logo.vue'
 import ColorMode from './ColorMode.vue'
-import Navbar from './Navbar.vue'
 import Search from '@/components/widgets/Search/index.vue'
 </script>
 
--- src/pages/[...slug].vue
+++ src/pages/[...slug].vue
@@ -1,29 +1,50 @@
 <template>
-  <div class="flex items-center justify-center m-auto px-6">
-    <div class="flex-1">
-      <ul v-for="item in articleList" :key="item">
-        <li>
-          <nuxt-link :to="item === 'index' ? '/' : item">{{ item }}</nuxt-link>
-        </li>
-      </ul>
+  <section class="relative lg:pt-8">
+    <div class="mx-auto sm:px-6 lg:px-8 px-4 max-w-7xl">
+      <div class="grid grid-cols-10 gap-4">
+        <div class="col-span-10">
+          <div class="flex flex-col relative lg:grid lg:grid-cols-10">
+            <aside
+              class="col-span-2 col-start-2 p-3 sticky top-14 border-b lg:border-none border-dashed backdrop-blur-md bg-white/75 dark:bg-black/75 lg:max-h-[calc(100vh-64px)]">
+              <button class="flex lg:hidden" type="button" @click="isShow = !isShow">分类</button>
+              <ul class="sticky top-14 lg:block max-h-[300px] overflow-y-auto" :class="isShow ? '' : 'hidden'">
+                <nuxt-link
+                  class="flex lg:justify-center px-1 py-2 cursor-pointer hover:bg-base-hover-color/40"
+                  v-for="item in articleList"
+                  :key="item"
+                  :to="item === 'index' ? '/' : item"
+                >{{ item }}
+                </nuxt-link>
+              </ul>
+            </aside>
+            <div class="col-span-10 lg:col-span-6">
+              <ContentDoc class="p-4">
+                <template #empty>
+                  <div>empty</div>
+                </template>
+                <template #not-found>
+                  <div>not found</div>
+                </template>
+              </ContentDoc>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <ContentDoc class="flex-[3]">
-      <template #empty>
-        <div>empty</div>
-      </template>
-      <template #not-found>
-        <div>not found</div>
-      </template>
-    </ContentDoc>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+const isShow = ref<boolean>(false)
 const articleList = []
 const modules = import.meta.glob('src/content/*.md', { as: 'raw' })
 for (const path in modules) {
+  // modules[path]().then((mod) => {
+  //   console.log(mod)
+  // })
   articleList.push(path.split('/content/')[1].split('.')[0])
 }
+
 </script>
 
 <style scoped lang="scss">
